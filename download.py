ace054d6934efebac9a6a6138b634307499664cf
chenshengda <chen_shengda@yeah.net>
2017-04-27 22:23:50 +0800

    链接爬虫

--- download.py
+++ download.py
@@ -1,17 +1,35 @@
 #coding:utf-8
 import urllib2
-def download(url,num_retries=2):
+import re
+def download(url,user_agent="wswp",num_retries=2):
 	print("Downloading:%s\n"%url)
+	headers={'user_agent':user_agent}
+	request=urllib2.Request(url,headers=headers)
 	try:
-		html=urllib2.urlopen(url).read()
+		html=urllib2.urlopen(request).read()
 	except urllib2.URLError,e:
 		print("download error:%s"%e.reason)
 		html=None
-		if hasattr(e,'code') and 500 <= e.code <= 600:
-			return download(url,num_retries-1)
+		if num_retries>0:
+			if hasattr(e,'code') and 500 <= e.code <= 600:
+				return download(url,num_retries-1)
+		else:
+			return None
 	return html
 	
-print(download("http://httpstat.us/500"))
+def link_crawl(seed_url,link_regex):
+	crawl_queue = [seed_url]
+	seen = set(crawl_queue)
+	while crawl_queue:
+		url = crawl_queue.pop()
+		html=download(url)
+		for link in get_links(html):
+			if re.match(link_regex,link):
+				link = urlparse.urljoin(seed_url,link)
+				if link not in seen:
+					seen.add(link)
+					crawl_queue.append(link)
+	
 
-		
+print(link_crawl("http://example.webscriping.com",'/(index|view)'))		
 	
